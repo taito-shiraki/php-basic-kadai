2f115886c0ee3e75e18b1dc09a0de330a64ed509
taito-shiraki <159087794+taito-shiraki@users.noreply.github.com>
2024-06-20 21:28:54 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>score</title>
+ </head>
+ 
+ <body>
+     <?php
+        $datas = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+        foreach($datas as $key => $value){
+            echo "{$key}:{$value}<br>";
+        }
+        ?>
+ </body>
+ 
+ </html>
